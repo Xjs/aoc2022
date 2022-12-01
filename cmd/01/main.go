e400d1b94f56759f917ed7ae9e2dd7b27c70008a
Jannis Andrija Schnitzer <jannis.schnitzer@infrontquant.com>
2022-12-01 16:41:48 +0100

    Add task number 2

--- cmd/01/main.go
+++ cmd/01/main.go
@@ -15,38 +15,84 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(solution)
+
+	solution2, err := aoc1task2(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(solution2)
 }
 
-// aoc1task reads the input, splits double newlines, and sums up the numbers in the individual lines in all chunks,
-// and returns the largest of the sums.
 func aoc1task1(r io.Reader) (int, error) {
+	return aoc1(r, 1)
+}
+
+func aoc1task2(r io.Reader) (int, error) {
+	return aoc1(r, 3)
+}
+
+// aoc1 reads the input, splits double newlines, and sums up the numbers in the individual lines in all chunks,
+// and returns the sum of the count largest of the sums.
+func aoc1(r io.Reader, count int) (int, error) {
 	s := bufio.NewScanner(r)
 
 	var sums []int
-	var largestIdx int
+	largestIdxs := make([]int, count)
+	for c := 0; c < count; c++ {
+		largestIdxs[c] = -1
+	}
 
-	for s.Scan() {
-		if len(sums) == 0 {
-			sums = append(sums, 0)
+	var inserted bool
+	insert := func(current int) {
+		if inserted {
+			return
 		}
-
-		current := len(sums) - 1
-
-		t := s.Text()
-		if t == "" {
-			sums = append(sums, 0)
-			continue
-		}
-		i, err := strconv.Atoi(t)
-		if err != nil {
-			return sums[largestIdx], err
-		}
-
-		sums[current] += i
-		if sums[current] > sums[largestIdx] {
-			largestIdx = current
+		inserted = true
+		for c := 0; c < count; c++ {
+			idx := largestIdxs[c]
+			if idx == -1 {
+				largestIdxs[c] = current
+				return
+			}
+			if sums[current] > sums[idx] {
+				for cc := count - 1; cc > c; cc-- {
+					largestIdxs[cc] = largestIdxs[cc-1]
+				}
+				largestIdxs[c] = current
+				return
+			}
 		}
 	}
 
-	return sums[largestIdx], s.Err()
+	var current int
+
+	for s.Scan() {
+		t := s.Text()
+		if t == "" {
+			sums = append(sums, 0)
+			insert(current)
+			continue
+		}
+
+		i, err := strconv.Atoi(t)
+		if err != nil {
+			return sums[largestIdxs[0]], err
+		}
+
+		inserted = false
+		if len(sums) == 0 {
+			sums = append(sums, 0)
+		}
+		current = len(sums) - 1
+
+		sums[current] += i
+	}
+	insert(current)
+
+	sum := 0
+	for _, li := range largestIdxs {
+		sum += sums[li]
+	}
+
+	return sum, s.Err()
 }
--- cmd/01/main_test.go
+++ cmd/01/main_test.go
@@ -7,17 +7,9 @@ import (
 )
 
 func Test_aoc1task1(t *testing.T) {
-	type args struct {
-		r io.Reader
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    int
-		wantErr bool
-	}{
-		{"example", args{strings.NewReader(
-			`1000
+	t.Parallel()
+
+	const exampleInput = `1000
 2000
 3000
 
@@ -30,18 +22,30 @@ func Test_aoc1task1(t *testing.T) {
 8000
 9000
 
-10000`,
-		)}, 24000, false},
+10000`
+
+	type args struct {
+		r     io.Reader
+		count int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{"example1", args{strings.NewReader(exampleInput), 1}, 24000, false},
+		{"example2", args{strings.NewReader(exampleInput), 3}, 45000, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := aoc1task1(tt.args.r)
+			got, err := aoc1(tt.args.r, tt.args.count)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("aoc1task1() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("aoc1() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
-				t.Errorf("aoc1task1() = %v, want %v", got, tt.want)
+				t.Errorf("aoc1() = %v, want %v", got, tt.want)
 			}
 		})
 	}
